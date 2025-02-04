fc399a9c46e1c46692d806be0dfd5ece4de30103
Jan <jameson4622007rgmail.com>
2025-02-04 11:58:27 +0100

    HOLAAAAAAAAA

---
+++ PROYECTO_FINAL/AirLeaks.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="Style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> All the War Thunder Leaks </title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+    <header>
+        <div class="top-header">
+            <a href="https://warthunder.com/es/"><img src="images\War-Thunder-Logo.png" width="300px"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php" target="_blank">Main</a></li>
+                <li><a href="TanksLeaks.php" target="_blank">Tanks leaks</a></li>
+                <li><a href="AirLeaks.php" target="_blank">AIr Leaks</a></li>
+                <li><a href="link.php" target="_blank">other Leaks</a></li>
+            </ul>
+        </nav>
+    </header>
+<main>
+<!-- Eurocopter Tiger -->
+    <section>
+    <h1>Eurocopter Tiger || 16/7/2021</h1>
+        <article> 
+            <h3>
+                Info for this leak is harder to come by, but it seems that the leak for the Tiger was about the armor layout for the vehicle.
+                <br>
+                <span><img src="images\EurocopterTiger.jpg" alt="" width="30%"></span>
+                <img src="images\EurocopterTiger-Evidence.png" alt="" width="50%">
+            </h3>
+        </article>
+    </section>
+
+<!-- Mig 29 -->
+<section>
+    <h1>MiG-29  || Dec 2022/Jan 2023 </h1>
+        <article> 
+            <h3>
+                the documents that we're leaked are technically only still classified in Russia, they're not so in former Pact countries like Poland or Hungary
+                <br>
+                <span><img src="images\mig29.png" alt="" width="40%"></span>
+                <img src="images\mig29_Evidence.png" alt="" width="40%">
+            </h3>
+        </article>
+    </section>
+
+<!-- F-15E -->
+<section>
+    <h1>F-15E  || 18/1/2023 </h1>
+        <article> 
+            <h3>
+                A host of software manuals for flight controls, navigation, targeting, and weapons got leaked by the same guy who leaked a document for the F-16.
+                <br>
+                <span><img src="images\F-15E.jpg" alt="" width="40%"></span>
+                <img src="images\F-15E_Evidence.jpg" alt="" width="20%">
+            </h3>
+        </article>
+    </section>
+</main>
+
+    <footer class="footer">
+        
+        <ul class="enlaces-legales">
+            <li><a href="https://legal.gaijin.net/" target="_blank">Términos y Condiciones</a></li>
+            <li><a href="https://legal.gaijin.net/privacypolicy/" target="_blank">Política de Privacidad</a></li>
+            <li><a href="https://legal.gaijin.net/termsofservice/" target="_blank">Condiciones de Uso</a></li>
+            <li><a href="https://support.gaijin.net/" target="_blank">Soporte al cliente</a></li>
+        </ul>
+            <img src="images/gaijin-entertainment-logo.png" alt="Gaijin Logo" class="logo-left">
+            <img src="images/War-Thunder-Logo-blanco.png" alt="War Thunder Logo" class="logo-center">
+    </footer>
+</body>
+</html>
