7b96ef65009dcd46d21fa0f09ba4447046a72693
Jan <jameson4622007rgmail.com>
2025-02-04 10:47:14 +0100

    owo

---
+++ PROYECTO FINAL/TanksLeaks.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="Style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> All the War Thunder Leaks </title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+    <header>
+        <div class="top-header">
+            <a href="https://warthunder.com/es/"><img src="images\War-Thunder-Logo.png" width="300px"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php" target="_blank">Main</a></li>
+                <li><a href="TanksLeaks.php" target="_blank">Tanks leaks</a></li>
+                <li><a href="AirLeaks.php" target="_blank">Air Leaks</a></li>
+                <li><a href="link.php" target="_blank">other Leaks</a></li>
+            </ul>
+        </nav>
+    </header>
+<main>
+    <section>
+     <h1>Challenger 2 || 17/7/2021</h1>
+        <article>
+            <h3>
+                A Challenger 2 tank commander posted excerpts from it's AESP to dispute it's modeling.
+                <br>
+                <span><img src="images\challenger2.jpg" alt="" width="40%"></span>
+                <img src="images\Challenger2_Evidence_1.JPG" alt="" width="40%">
+            </h3>
+        </article>
+            
+    </section>
+
+<!--Leclerc -->
+    <section>
+    <h1>LeClerc || 6/10/2021</h1>
+        <article> 
+            <h3>
+                The turret rotation speed got leaked for the LeClerc by a crewman onto the forums.
+                <br>
+                <span><img src="images\LeClerc.jpg" alt="" width="40%"></span>
+                <img src="images\LeClerc_Evidence.jpg" alt="" width="30%">
+            </h3>
+    <!-- Button 1 -->
+    <a href="https://ukdefencejournal.org.uk/classified-tank-specs-leaked-on-war-thunder-game-forums-again/" 
+       class="button button-1" 
+       target="_blank">Uk de Fence Journal
+
+    <!-- Button 2 -->
+    <a href="https://www.reddit.com/r/Warthunder/comments/q2hk86/just_reading_leclerc_in_wikipedia_and_then_i/" 
+       class="button button-2" 
+       target="_blank">Reddit Post</a>
+        </article>
+    </section>
+
+<!--TYPE 99-->
+    <section>
+        <h1>Type-99 || 5/6/2022 </h1>
+            <article> 
+                <h3>
+                    Assorted documentation on the Chinese Type-99 MBT and it's ammunition was leaked by a Chinese player onto the forums
+                    <br>
+                    <span><img src="images\type99.jpg" alt="" width="50%"></span>
+                </h3>
+    <!-- Button 1 -->
+    <a href="https://taskandpurpose.com/news/war-thunder-player-leaks-chinese-tank-secrets/" 
+       class="button button-1" 
+       target="_blank">Task and Purpose
+
+    <!-- Button 2 -->
+    <a href="https://www.eurasiantimes.com/classified-info-on-chinas-main-battle-tank-type-99-leaked-online/" 
+       class="button button-2" 
+       target="_blank">The Eurasian times</a>
+
+            </article>
+    </section>
+
+<!--M2A2 Bradley-->
+<section>
+    <h1>M2A2 Bradley || 19/12/2023 </h1>
+        <article> 
+            <h3>
+                A leak for the M2A2 Bradley happened on the WarThunder "Machinery of War" forum. The documents being a set of export restricted pages going over commander's hatch, turret assembly and spall liner. The original posting happened on December 8th according to the linked article.
+                <br>
+            <span><img src="images\M2A2-bradley.jpg" alt="" width="50%"></span>
+            </h3>
+<!-- Button 1 -->
+    <a href="https://taskandpurpose.com/news/leaked-bradley-documents-war-thunder-forums-again/" 
+        class="button button-1" 
+        target="_blank">Task and Purpose
+    </a>
+        </article>
+</section>
+
+</main>
+
+    <footer class="footer">
+        
+        <ul class="enlaces-legales">
+            <li><a href="https://legal.gaijin.net/" target="_blank">Términos y Condiciones</a></li>
+            <li><a href="https://legal.gaijin.net/privacypolicy/" target="_blank">Política de Privacidad</a></li>
+            <li><a href="https://legal.gaijin.net/termsofservice/" target="_blank">Condiciones de Uso</a></li>
+            <li><a href="https://support.gaijin.net/" target="_blank">Soporte al cliente</a></li>
+        </ul>
+            <img src="images/gaijin-entertainment-logo.png" alt="Gaijin Logo" class="logo-left">
+            <img src="images/War-Thunder-Logo-blanco.png" alt="War Thunder Logo" class="logo-center">
+    </footer>
+</body>
+</html>
---
+++ PROYECTO FINAL/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="Style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> All the War Thunder Leaks </title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+    <header>
+        <div class="top-header">
+            <a href="https://warthunder.com/es/"><img src="images\War-Thunder-Logo.png" width="300px"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php" target="_blank">Main</a></li>
+                <li><a href="TanksLeaks.php" target="_blank">Tanks leaks</a></li>
+                <li><a href="AirLeaks.php" target="_blank">Air Leaks</a></li>
+                <li><a href="link.php" target="_blank">other Leaks</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <h2>Welcome to 
+                All the War Thunder Leaks List
+            </h2>
+        </section>
+        <section class="intro">
+            <div class="container">
+                <h1>Discover the Secrets of War Thunder</h1>
+                <h3>
+                    Welcome to the ultimate hub for War Thunder enthusiasts! 
+                </h3>
+                <h5>
+                    Here, you'll find a comprehensive timeline of all the significant leaks 
+                    that have captured the community's attention. 
+                    Whether you're a seasoned player or new to the battlefield, 
+                    this is your one-stop destination to explore exclusive content and stay informed.
+                </h5>
+                <img src="images\Background_WarThunder.jpg" alt="Background_WarThunder" width="70%">
+                <h3>
+                    Delve into the details, analyze the reveals, 
+                    and get ready to immerse yourself in the evolving world of War Thunder.
+                </h3>
+                <br>
+            </div>
+        </section>
+    </main>
+
+    <footer class="footer">
+        
+          
+          <ul class="enlaces-legales">
+            <li><a href="https://legal.gaijin.net/" target="_blank">Términos y Condiciones</a></li>
+            <li><a href="https://legal.gaijin.net/privacypolicy/" target="_blank">Política de Privacidad</a></li>
+            <li><a href="https://legal.gaijin.net/termsofservice/" target="_blank">Condiciones de Uso</a></li>
+            <li><a href="https://support.gaijin.net/" target="_blank">Soporte al cliente</a></li>
+          </ul>
+          <img src="images/gaijin-entertainment-logo.png" alt="Gaijin Logo" class="logo-left">
+          <img src="images/War-Thunder-Logo-blanco.png" alt="War Thunder Logo" class="logo-center">
+      </footer>
+</body>
+</html>
